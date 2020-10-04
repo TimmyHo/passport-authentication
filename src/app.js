eb9186c6dbe3cf214d430226316b2683dfa5f840
Tim Hsiung <timothy.hsiung@gmail.com>
2020-10-04 08:08:09 +0800

    Added mongo db connection and created a user which finds or creates a google id user

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const session = require('express-session');
 
 const connect = require('connect-ensure-login');
 
+const mongoose = require('./db/mongoose');
 const User = require('./models/user');
 
 const app = express();
@@ -42,29 +43,57 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 passport.serializeUser((user, done) => {
+    console.log('serializing user')
     done(null, user.id);
 });
   
 passport.deserializeUser((id, done) => {
+    console.log('deserializing user')
     User.findById(id, (err, user) => {
         done(err, user);
     });
 });
 
-passport.use(new LocalStrategy(
-    (username, password, done) => {
-        console.log(`trying to authenticate: ${username}, ${password} `);
-        User.findByUsername(username, (err, user) => {
-        if (err) { return done(err); }
-        if (!user) {
-            return done(null, false, { message: 'Incorrect username.' });
-        }
-        if (user.password !== password) {
-            return done(null, false, { message: 'Incorrect password.' });
-        }
-        return done(null, user);
-        });
-    }
+// passport.use(new LocalStrategy(
+//     (username, password, done) => {
+//         console.log(`trying to authenticate: ${username}, ${password} `);
+//         User.findByUsername(username, (err, user) => {
+//         if (err) { return done(err); }
+//         if (!user) {
+//             return done(null, false, { message: 'Incorrect username.' });
+//         }
+//         if (user.password !== password) {
+//             return done(null, false, { message: 'Incorrect password.' });
+//         }
+//         return done(null, user);
+//         });
+//     }
+// ));
+
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+
+// Use the GoogleStrategy within Passport.
+//   Strategies in Passport require a `verify` function, which accept
+//   credentials (in this case, an accessToken, refreshToken, and Google
+//   profile), and invoke a callback with a user object.
+
+// uses the desktop client ouath client id and secret 
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    console.log('this is working');
+    console.log(accessToken);
+    console.log(refreshToken);
+    console.log(profile)
+
+       User.findOrCreate({ email: profile.emails[0].value, type: 'google', googleId: profile.id }, function (err, user) {
+         return done(err, user);
+       });
+    done(null, profile);
+  }
 ));
 
 app.get('/', (req, res) => {
@@ -78,7 +107,7 @@ app.get('/login',
     res.render('login');
 });
 
-app.post('/login', passport.authenticate('local', { successRedirect: '/userInfo', failureRedirect: '/login' }));
+// app.post('/login', passport.authenticate('local', { successRedirect: '/userInfo', failureRedirect: '/login' }));
 
 // this only renders on a post to login which should actually redirect to userinfo
 app.post('/login', passport.authenticate('local'), (req, res) => {
@@ -90,6 +119,26 @@ app.get('/userInfo', connect.ensureLoggedIn('/login'),   (req, res) => {
     console.log(req.user);
     res.render('userInfo', {user: req.user});
 })
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/userinfo.profile', 'openid'] }));
+
+// GET /auth/google/callback
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  If authentication fails, the user will be redirected back to the
+//   login page.  Otherwise, the primary route function function will be called,
+//   which, in this example, will redirect the user to the home page.
+// app.get('/auth/google/callback', 
+//   passport.authenticate('google', { failureRedirect: '/login' }),
+//   function(req, res) {
+//     res.redirect('/');
+//   });
+
+app.get('/auth/google/callback', passport.authenticate("google"), (req, res) => {
+    console.log(req.user);
+    res.send(req.user);
+});
+
 
 app.listen(port,  () => {
     console.log('Server is up on port '+port);
--- src/models/user.js
+++ src/models/user.js
@@ -1,29 +1,105 @@
-var records = [
-    { id: 1, username: 'jack', password: 'a', displayName: 'Jack', emails: [ { value: 'jack@example.com' } ] }
-  , { id: 2, username: 'jill', password: 'b', displayName: 'Jill', emails: [ { value: 'jill@example.com' } ] }
-];
+const mongoose = require('mongoose');
 
-exports.findById = (id, cb) => {
-    process.nextTick(() => {
-        var idx = id - 1;
-        if (records[idx]) {
-            cb(null, records[idx]);
-        } else {
-            cb(new Error('User ' + id + ' does not exist'));
-        }
-    });
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String
+    },
+    googleId: {
+        type: String
+    }
+});
+
+// var records = [
+//     { id: 1, username: 'jack', password: 'a', displayName: 'Jack', emails: [ { value: 'jack@example.com' } ] }
+//   , { id: 2, username: 'jill', password: 'b', displayName: 'Jill', emails: [ { value: 'jill@example.com' } ] }
+// ];
+
+// exports.findById = (id, cb) => {
+//     process.nextTick(() => {
+//         var idx = id - 1;
+//         if (records[idx]) {
+//             cb(null, records[idx]);
+//         } else {
+//             cb(new Error('User ' + id + ' does not exist'));
+//         }
+//     });
+// }
+
+// exports.findByUsername = (username, cb) => {
+//     process.nextTick(() => {
+//         for (var i = 0, len = records.length; i < len; i++) {
+//             var record = records[i];
+//             if (record.username === username) {
+//                 console.log('found record: ');
+//                 console.log(record);
+//                 return cb(null, record);
+//             }
+//         }
+//         return cb(null, null);
+//     });
+// }
+
+
+const User = mongoose.model('User', userSchema);
+
+// User.findById = (id, cb) => {
+    
+//     // process.nextTick(() => {
+//         var idx = id - 1;
+//         if (records[idx]) {
+//             cb(null, records[idx]);
+//         } else {
+//             cb(new Error('User ' + id + ' does not exist'));
+//         }
+//     // });
+// }
+
+User.findByEmail = async (email, cb) => {
+    // process.nextTick(() => {
+    //     for (var i = 0, len = records.length; i < len; i++) {
+    //         var record = records[i];
+    //         if (record.username === username) {
+    //             console.log('found record: ');
+    //             console.log(record);
+    //             return cb(null, record);
+    //         }
+    //     }
+    //     return cb(null, null);
+    // });
+    const res = await User.findOne({email});
+
+    if (!res) {
+        return cb('User not found');
+    }
+    else {
+        return cb(null, res);
+    }
 }
 
-exports.findByUsername = (username, cb) => {
-    process.nextTick(() => {
-        for (var i = 0, len = records.length; i < len; i++) {
-            var record = records[i];
-            if (record.username === username) {
-                console.log('found record: ');
-                console.log(record);
-                return cb(null, record);
+User.findOrCreate = async(payload, cb) => {
+    User.findByEmail(payload.email, async (err, user) => {
+
+        if (!user) {
+            let newUser = null;
+            if (payload.type === 'google') {
+                newUser = new User({
+                    email: payload.email,
+                    googleId: payload.googleId
+                })
             }
+
+
+            await newUser.save();
+            cb(null, newUser);
+        } else {
+            cb(null, user);
         }
-        return cb(null, null);
-    });
+    })   
 }
+
+module.exports = User;
