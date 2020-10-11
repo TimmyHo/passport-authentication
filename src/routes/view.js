bcc1d3275fcebd1f463968af9e02e0aeff858d51
Tim Hsiung <timothy.hsiung@gmail.com>
2020-10-12 00:06:25 +0800

    updated ui so that it looks nicer and has various navbar items based on logged in status

--- src/routes/view.js
+++ src/routes/view.js
@@ -4,25 +4,25 @@ const authMiddleware = require('../middleware/auth');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('welcome');
+    res.render('welcome', {signedIn: req.isAuthenticated()});
 })
 
 router.get('/signup', (req, res) => {
     err = req.flash('error');
-    res.render('sign-up', {err});
+    res.render('sign-up', {signedIn: req.isAuthenticated(), err});
 });
 
 router.get('/signin', (req, res) => {
     err = req.flash('error');
-    res.render('sign-in', {err});
+    res.render('sign-in', {signedIn: req.isAuthenticated(), err});
 });
 
-router.get('/user/me', authMiddleware.isSignedIn,  (req, res) => {
-    res.render('user-info', {user: req.user});
+router.get('/user/me', authMiddleware.isSignedIn, (req, res) => {
+    res.render('user-info', {signedIn: req.isAuthenticated(), user: req.user});
 });
 
 router.get('/secret', authMiddleware.isSignedIn, (req, res) => {
-    res.render('secret');
+    res.render('secret',{signedIn: req.isAuthenticated()    });
 });
 
 module.exports = router;
